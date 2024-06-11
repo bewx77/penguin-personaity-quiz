5704a50e00696b08b457a0bf72b52204f13e323d
bewx77 <112966395+bewx77@users.noreply.github.com>
2024-06-11 15:46:35 +0800

    Create questions.js

---
+++ models/questions.js
@@ -0,0 +1,114 @@
+const questions = [
+    {
+        "question": "Oh! What happened? You found out you woke up as a penguin… After the initial shock, you start to collect your thoughts:",
+        "options": {
+            "A": { 
+                "desc": "“Oh no what is going on? I wonder if it feels the same walking in flippers...”",
+                "matches": ["ESFJ", "ISFJ", "ESTJ", "ISTJ", "ENFP", "INFP", "ENTP", "INTP"]
+            },
+            "B": { 
+                "desc": "Interesting... Let’s find out what I can do as a penguin...",
+                "matches": ["ENFJ", "INFJ", "ESFP", "ISFP", "ESTP", "ISTP", "ENTJ", "INTJ"]
+            }
+        },
+        "image": "question_1.jpeg"
+    },
+    {
+        "question": "Knock knock! You open the door to 4 other penguins. “Hey! Hurry up, we are going to be late for the annual cooking competition!\"",
+        "options": {
+            "A": { 
+                "desc": "Woah a cooking competition? Let’s go!",
+                "matches": ["ESTP", "ESFP", "ENTP", "ENFP", "ISTP", "ISFP", "INTP", "INFP"]
+            },
+            "B": { 
+                "desc": "Er what is going on? What competition? What are we going to cook?",
+                "matches": ["ESTJ", "ESFJ", "ENTJ", "ENFJ", "ISTJ", "ISFJ", "INTJ", "INFJ"]
+            }
+        },
+        "image":"question_2.jpeg"
+    },
+    {
+        "question": "Before you know it, your friends dragged you to the townhall to begin the competition. “Hey seems like each one of us has to pick a task: Washing, Cutting, Cooking, Tasting, Plating.\"",
+        "options": {
+            "A": { 
+                "desc": "You immediately picked the task you know you are good at.",
+                "matches": ["ESTJ", "ISTJ", "ENFP", "INFP", "ESFP", "ISFP", "ENTJ", "INTJ"]
+            },
+            "B": { 
+                "desc": "I will just pick the tasks that is leftover. The rest can pick first!",
+                "matches": ["ESFJ", "ISFJ", "ENFJ", "ENFJ", "ESTP", "ISTP", "ENTP", "INTP"]
+            }
+        },
+        "image":"question_3.jpeg"
+    },
+    {
+        "question": "After hours of cooking, it was time for the judging. Your team huddled together, feeling hopeful. “Hmm the salmon was… raw. I can't eat this.” Suddenly, you see your teammate tearing up...",
+        "options": {
+            "A": { 
+                "desc": "You immediately gave your teammate a hug. Seing him makes you want to tear up too, knowing everyone's hard work in this.",
+                "matches": ["ESFJ", "ESFP", "ENFJ", "ENFP", "ISFJ", "ISFP", "INFJ", "INFP"]
+            },
+            "B": { 
+                "desc": "You try to comfort your teammate by telling him that this is not the end of the world and there will be other cooking competitions next time!",
+                "matches": ["ESTJ", "ESTP", "ENTJ", "ENTP", "ISTJ", "ISTP", "INTJ", "INTP"]
+            }
+        },
+        "image":"question_4.jpeg"
+    },
+    {
+        "question": "Despite the mistake, your group decided to head down to the nearest igloo bar to raise everyone's spirits! Since the bar was full, the group had to split:",
+        "options": {
+            "A": { 
+                "desc": "You sit with the penguins that were looking back on the funny moments that happened during the competition.",
+                "matches": ["ESTJ", "ESTP", "ESFJ", "ESFP", "ISTJ", "ISTP", "ISFJ", "ISFP"]
+            },
+            "B": { 
+                "desc": "You chill with the penguins that talked about absolutely anything, even abstract topics like the possibilities of penguins ever being humans.",
+                "matches": ["ENTJ", "ENTP", "ENFJ", "ENFP", "INTJ", "INTP", "INFJ", "INFP"]
+            }
+        },
+        "image":"question_5.jpeg"
+    },
+    {
+        "question": "The night was great and you felt like the drinks you had were awesome too. Upon searching the Penguin Reviews, this bar surprisingly only had a 2.9 stars rating.",
+        "options": {
+            "A": { 
+                "desc": "Oh did i got lucky and ordered something good? Maybe the drinks weren't as good as i thought?",
+                "matches": ["ESTJ", "ISTJ", "ENFP", "INFP", "ESFP", "ISFP", "ENTJ", "INTJ"]
+            },
+            "B": { 
+                "desc": "Penguins are weird, the drinks were good though.",
+                "matches": ["ESFJ", "ISFJ", "ENFJ", "ENFJ", "ESTP", "ISTP", "ENTP", "INTP"]
+            }
+        },
+        "image":"question_6.jpeg"
+    },
+    {
+        "question": "After an eventful day, you bid farewell to the new friends you had made and went back to tug yourself into bed. Staring at the icy ceiling:",
+        "options": {
+            "A": { 
+                "desc": "You play all the different scenarios in your mind about how tomorrow might go",
+                "matches": ["ESFJ", "ISFJ", "ESTJ", "ISTJ", "ENFP", "INFP", "ENTP", "INTP"]
+            },
+            "B": { 
+                "desc": "You play a singular scenario in your mind about how tomorrow would most likely go",
+                "matches": ["ENFJ", "INFJ", "ESFP", "ISFP", "ESTP", "ISTP", "ENTJ", "INTJ"]
+            }
+        },
+        "image":"question_7.jpeg"
+    },
+    {
+        "question": "Ring ring… your alarm rang. And suddenly you are back home and back to your human self!",
+        "options": {
+            "A": { 
+                "desc": "Damn… i got to go and tell as many people about my crazy adventure",
+                "matches": ["ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP", "ENFJ", "ENFP"]
+            },
+            "B": { 
+                "desc": "Oh my… let me think about what has just happened and maybe find some of my close friends to talk about what just had happened...",
+                "matches": ["ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP"]
+            }
+        },
+        "image":"question_8.jpeg"
+    }
+]
