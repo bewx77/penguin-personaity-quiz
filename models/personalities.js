e17dfd0e37e1b7e9329f32c3be17695749e1f60f
bewx77 <112966395+bewx77@users.noreply.github.com>
2024-06-11 15:45:58 +0800

    Create personalities.js

---
+++ models/personalities.js
@@ -0,0 +1,131 @@
+const personalities = [
+    {
+      "mbti": "ISTJ",
+      "image": "penguin_istj.png",
+      "name": "Leo, the Logical Penguin",
+      "traits": ["very good memory", "cite your sources but trusts gossips", "skeptical of new people", "follow the rule"],
+      "compatible": ["ESTP", "ISTP"],
+      "notCompatible": ["ENFP", "INFP"]
+    },
+    {
+      "mbti": "ESTJ",
+      "image": "penguin_estj.png",
+      "name": "Ernest, the Executive Penguin",
+      "traits": ["very loyal", "straight-forward", "quick-to-anger", "ambitious and hardworking"],
+      "compatible": ["ISTP", "ESTP"],
+      "notCompatible": ["INFP", "ENFP"]
+    },
+    {
+      "mbti": "ISFJ",
+      "image": "penguin_isfj.png",
+      "name": "Danielle, the Defending Penguin",
+      "traits": ["thoughtful and empathetic", "remembers the little details", "behind-the-scene doers", "reliable"],
+      "compatible": ["ESFP", "ISFP"],
+      "notCompatible": ["ENTP", "INTP"]
+    },
+    {
+      "mbti": "ESFJ",
+      "image": "penguin_esfj.png",
+      "name": "Dylan, the Diplomatic Penguin",
+      "traits": ["high loyalty", "seeing someone feels better makes them happy", "always remember the little guy", "light-hearted and appreciate simple things"],
+      "compatible": ["ISFP", "ESFP"],
+      "notCompatible": ["INTP", "ENTP"]
+    },
+    {
+      "mbti": "ESTP",
+      "image": "penguin_estp.png",
+      "name": "Elliot, the Entrepreneurial Penguin",
+      "traits": ["afraid of being alone", "work hard play hard", "live in the moment and very observant", "very sociable"],
+      "compatible": ["ISTJ", "ESTJ"],
+      "notCompatible": ["INFJ", "ENFJ"]
+    },
+    {
+      "mbti": "ISTP",
+      "name": "Heather, the Hotshot Penguin",
+      "image": "penguin_istp.png",
+      "traits": ["straightforward until they hurt someone's feelings", "listens more and talks less", "bad at sticking to really detailed plans", "really good at hands-on work"],
+      "compatible": ["ESTJ", "ISTJ"],
+      "notCompatible": ["ENFJ", "INFJ"]
+    },
+    {
+      "mbti": "ESFP",
+      "image": "penguin_esfp.png",
+      "name": "Eve, the Entertainer Penguin",
+      "traits": ["spontaneous but detail-oriented", "loves sensory experiences (e.g. lights and sounds)", "warm and understanding", "figure things out as they go"],
+      "compatible": ["ISFJ", "ESFJ"],
+      "notCompatible": ["INTJ", "ENTJ"]
+    },
+    {
+      "mbti": "ISFP",
+      "image": "penguin_isfp.png",
+      "name": "Adam, the Adventurer Penguin",
+      "traits": ["emotionally intense but quiet about it", "put a lot of thought into everything", "observant of body language cues", "go with the flow but also individualistic"],
+      "compatible": ["ESFJ", "ISFJ"],
+      "notCompatible": ["ENTJ", "INTJ"]
+    },
+    {
+      "mbti": "ENTJ",
+      "image": "penguin_entj.png",
+      "name": "Calvin, the Commander Penguin",
+      "traits": ["has entire life planned out", "high confidence", "no nonsense", "never ending hustle"],
+      "compatible": ["INTP", "ENTP"],
+      "notCompatible": ["ISFP", "ESFP"]
+    },
+    {
+      "mbti": "INTJ",
+      "image": "penguin_intj.png",
+      "name": "Aidan, the Architect Penguin",
+      "traits": ["frustrated at others incompetence", "understand concepts easily", "could not care less about social norms", "very stubborn"],
+      "compatible": ["ENTP", "INTP"],
+      "notCompatible": ["ESFP", "ISFP"]
+    },
+    {
+      "mbti": "INTP",
+      "image": "penguin_intp.png",
+      "name": "Thea, the Thinker Penguin",
+      "traits": ["short sentences or full-on literature essay", "sudden burst of productivity", "like to play “devil's advocate”", "awkward"],
+      "compatible": ["ENTJ", "INTJ"],
+      "notCompatible": ["ESFJ", "ISFJ"]
+    },
+    {
+      "mbti": "ENTP",
+      "image": "penguin_entp.png",
+      "name": "Dante, the Debater Penguin",
+      "traits": ["thirst for knowledge", "the more changes the better", "non-judgemental", "might seem introverted"],
+      "compatible": ["INTJ", "ENTJ"],
+      "notCompatible": ["ISFJ", "ESFJ"]
+    },
+    {
+      "mbti": "ENFJ",
+      "image": "penguin_enfj.png",
+      "name": "Pam, the Protagonist Penguin",
+      "traits": ["extremely charismatic", "blend in to any social group", "try to get everyone to participate", "always listening and helping"],
+      "compatible": ["INFP", "ENFP"],
+      "notCompatible": ["ISTP", "ESTP"]
+    },
+    {
+      "mbti": "INFJ",
+      "image": "penguin_infj.png",
+      "name": "Aaron, the Advocating Penguin",
+      "traits": ["introspective and enjoys deep thinking", "strong empathy", "independent and creative", "door slams people after being betrayed"],
+      "compatible": ["ENFP", "INFP"],
+      "notCompatible": ["ESTP", "ISTP"]
+    },
+    {
+      "mbti": "ENFP",
+      "image": "penguin_enfp.png",
+      "name": "Marisa, the Mediating Penguin",
+      "traits": ["massive enthusiasm with friends", "thoughts all over the place", "90+ tabs in browsers", "sometimes hesitant but never regret going to a party"],
+      "compatible": ["INFJ", "ENFJ"],
+      "notCompatible": ["ISTJ", "ESTJ"]
+    },
+    {
+      "mbti": "INFP",
+      "image": "penguin_infp.png",
+      "name": "Caleb, the Campaigning Penguin",
+      "traits": ["emotionally sensitive", "feeling misunderstood", "getting caught up in daydreams", "having a deep well of emotions that they keep to themselves"],
+      "compatible": ["ENFJ", "INFJ"],
+      "notCompatible": ["ESTJ", "ISTJ"]
+    }
+  ]
+  
